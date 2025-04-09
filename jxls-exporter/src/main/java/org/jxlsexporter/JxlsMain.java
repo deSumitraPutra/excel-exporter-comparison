f106a9a50b4a9fca388b71e61f9f46a39eede053
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2025-04-09 10:56:45 +0800

    add .env to ignore and rename main class

--- jxls-exporter/src/main/java/org/jxlsexporter/JxlsMain.java
+++ jxls-exporter/src/main/java/org/jxlsexporter/JxlsMain.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class JxlsMain {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(JxlsMain.class, args);
     }
 }
